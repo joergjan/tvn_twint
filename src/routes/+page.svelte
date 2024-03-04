13a911d729013f196d34401034bbee892a620663
Jan Jörg <jan.joerg.jj@gmail.com>
2024-03-04 23:42:54 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,12 @@
-<script lang="ts">
-    import { currentPage } from "$lib/stores";
-    import { onMount } from "svelte";
-
-    let paymentAmount: number;
-</script>
-
 <svelte:head>
     <title>TV Nussbaumen</title>
     <meta name="robots" content="noindex, nofollow" />
 </svelte:head>
 
-<h1>TWINT</h1>
-
-<div class="flex flex-col items-center">
-    <input
-        type="number"
-        placeholder="Betrag eingeben"
-        bind:value={paymentAmount}
-        pattern="[0-9]*"
-        class="border-2 border-gray-300 p-2 mb-4 text-center"
-    />
-    <form action="?/pay">
-        <input type="number" hidden bind:value={paymentAmount} />
-    </form>
-</div>
+<iframe
+    class="w-screen h-screen"
+    title="raisenow twint"
+    src="https://pay.raisenow.io/pxqds?terminal_mode=true&lng=en"
+    frameborder="0"
+>
+</iframe>
