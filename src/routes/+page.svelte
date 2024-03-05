de075c3098f3503d8c5b7125f383a085f42cce99
Jan Jörg <jan.joerg.jj@gmail.com>
2024-03-05 10:19:20 +0100

    lang update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <iframe
     class="w-screen h-screen"
     title="raisenow twint"
-    src="https://pay.raisenow.io/pxqds?terminal_mode=true&lng=en"
+    src="https://pay.raisenow.io/pxqds?terminal_mode=true&lng=de"
     frameborder="0"
 >
 </iframe>
